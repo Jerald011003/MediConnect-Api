89cfa5273937dd93bceeecaab217ef415bf64609
jeraldfrancis-fullstack <coninegaming@gmail.com>
2025-09-24 02:04:59 +0800

    delete auth contexts

--- src/app/authentication/layout.tsx
+++ src/app/authentication/layout.tsx
@@ -1,4 +1,3 @@
-import { AuthProvider } from "@/contexts/AuthContext";
 
 export default function AuthLayout({
   children,
@@ -6,12 +5,10 @@ export default function AuthLayout({
   children: React.ReactNode
 }) {
   return (
-    <AuthProvider>
       <div className="min-h-screen flex items-center justify-center ">
         <div className="w-full max-w-md">
           {children}
         </div>
       </div>
-    </AuthProvider>
   )
 }
